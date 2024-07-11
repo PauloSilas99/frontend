4791f7cddc26eb93f499c2a395e5d2e7c7625533
PauloSilas99 <fonsecasodreps@hotmail.com>
2024-07-11 10:58:39 -0300

    teste de commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ function Skills(){
     return (
         <div className="divSkills">
             <h3 className='textoSkills'>Skills</h3>
-
+            <p>AQUI VAI FICAR OS ÍCONES DAS TEXNOLOGIAS</p>
         </div>
     )
 }
