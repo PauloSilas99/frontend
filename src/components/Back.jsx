aa029385f51578dec9b54f42acba79cea674f1cc
PauloSilas99 <fonsecasodreps@hotmail.com>
2024-07-14 20:25:11 -0300

    modificacoes na apresentacao dos projetos

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,8 +1,33 @@
 
+import node from '../assets/Group 5.png'
 function Back(){
     return(
         <div>
-
+            <div>
+                <div>
+                    <h3>Lista</h3>
+                    <div>
+                        <p>Lista com as funções de Criar,Visualizar,Editar e Deletar.</p>
+                    </div>
+                    <div>
+                        <p>Desenvolvido com : Node Js</p>
+                        <img src={node} alt="node" />
+                    </div>
+                </div>
+                <div>
+                    <div className='divImg'>
+                       
+                    </div>
+                    <div className='linksProjeto'>
+                        {/* <div className='linkBtn'>
+                            <a href="">Ver Projeto</a>
+                        </div> */}
+                        <div className='linkBtn'>
+                            <a href="">Repositório</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/FrontEnd.jsx
+++ src/components/FrontEnd.jsx
@@ -1,6 +1,7 @@
 import '../components/Front.css'
 import reactjs from '../assets/Vector.png'
 import imgReact from '../assets/CapturaProjetoReact 1.svg'
+import ProjetoApp from '../assets/projeto-app.png'
 
 function Front(){
     return (
@@ -33,25 +34,22 @@ function Front(){
 
             <div className="projetoFront">
                 <div className='divTexto'>
-                    <h3>Regitro de Motos</h3>
+                    <h3>App Registro de Débito</h3>
                     <div className='divDescricao'>
-                        <p>Lista de registro de Motos com nome,valor,cor e opção de estoque.Cada moto adicionada pode ser editada ou excluída</p>
+                        <p>Esse Aplicativo visa melhorar o registro de débitos de seu usuário. Com um visual fácil e prático, os registros ficam armazenasdos com um fácil manuseio.</p>
                     </div>
                     <div className='tecnologia'>
-                        <p>Desenvolvido com : React.Js</p>
+                        <p>Desenvolvido com : React Native + Expo</p>
                         <img src={reactjs} alt="reactjs" />
                     </div>
                 </div>
                 <div className='divImgLinks'>
-                    <div className='divImg'>
-                        <img src={imgReact} alt="imgReact" />
+                    <div className='divImg projApp'>
+                        <img src={ProjetoApp} alt="ProjetoApp" />
                     </div>
                     <div className='linksProjeto'>
                         <div className='linkBtn'>
-                            <a href="https://teste-motoca-systems-front-6s7c10yjg-paulo-silas-projects.vercel.app/">Ver Projeto</a>
-                        </div>
-                        <div className='linkBtn'>
-                            <a href="https://github.com/PauloSilas99/teste-motocaSystems-frontEnd">Repositório</a>
+                            <a href="https://github.com/PauloSilas99/AppCobran-a.git">Repositório</a>
                         </div>
                     </div>
                 </div>
